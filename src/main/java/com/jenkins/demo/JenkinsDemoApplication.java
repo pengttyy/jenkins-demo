d5f3f574a671b76263e6a04aefc4298268429f6c
pk <pengttyy@gmail.com>
2018-08-15 17:07:33 +0800

    添加启动执行类

--- src/main/java/com/jenkins/demo/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/demo/JenkinsDemoApplication.java
@@ -1,12 +1,23 @@
 package com.jenkins.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+    @Component
+    public class Runner implements CommandLineRunner {
+
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println("test jenkins demo!");
+        }
     }
 }
